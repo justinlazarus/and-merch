1b1e855258e85b5e838f9d394529c2e829e38e39
Justin Lazarus <justin.lazarus@gmail.com>
2011-07-03 20:39:48 +0800

    ZZ

--- src/costco/merchandise/itemhelper/scripts/rip_legacy.py
+++ src/costco/merchandise/itemhelper/scripts/rip_legacy.py
@@ -1,11 +1,19 @@
 import legacy 
 import settings
 import os
+import prog
 
 # Import legacy tables from .csv files on the MicroSD card
 os.chdir(settings.IMPORT_PATH)
-for entry in os.listdir(settings.IMPORT_PATH):
+dirlist = os.listdir(settings.IMPORT_PATH)
+pbar = prog.ProgressBar(maxval=len(dirlist)).start()
+count = 0
+
+for entry in dirlist:
+    count +=1
     entry_name, entry_extension = os.path.splitext(entry)
     if entry_extension == '.csv':
         legacy.Table(entry, settings.ENCODING, entry_name).create()
-        print("%s table created succesfully" % entry_name)
+        pbar.update(count)
+pbar.finish()
+print
